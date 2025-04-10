971de1c3d7378c103f09921dc4cc4de45ba61819
Michael Grieco <mag2346@columbia.edu>
2025-04-10 19:17:47 +0000

    Implement plle2_drp in the tile to replace the DCO

--- tools/socketgen/templates/noc_interface.vhd
+++ tools/socketgen/templates/noc_interface.vhd
@@ -42,7 +42,8 @@ use std.textio.all;
     has_l2         : integer := 1);
   port (
     rst       : in  std_ulogic;
-    clk       : in  std_ulogic;
+    tile_clk  : in  std_ulogic;
+    acc_clk   : in  std_ulogic;
     local_y   : in  local_yx;
     local_x   : in  local_yx;
     tile_id   : in  integer range 0 to CFG_TILES_NUM - 1;
@@ -109,6 +110,8 @@ use std.textio.all;
 end;
 
 -- <<architecture>>
+
+  signal clk : std_ulogic;
 
   -- Device ID and revision numner
   constant vendorid      : vendor_t               := VENDOR_SLD;
@@ -221,7 +224,7 @@ end;
   signal acc_flush_done             : std_ulogic;
   -- Register control, interrupt and monitor signals
   signal mon_dvfs_feedthru : monitor_dvfs_type;
-  
+
   constant ahbslv_proxy_hindex : hindex_vector(0 to NAHBSLV - 1) := (
     others => 0);
 
@@ -272,8 +275,10 @@ end;
 
 begin
 
+  clk <= acc_clk;
+
   interrupt_ack_rdreq <= '0';
-  
+
   -- <<accelerator_instance>>
 
   l2_gen: if has_l2 /= 0 generate
@@ -365,7 +370,8 @@ begin
       tlb_entries        => tlb_entries)
     port map (
       rst                           => rst,
-      clk                           => clk,
+      tile_clk                      => clk,
+      acc_clk                       => clk,
       local_y                       => local_y,
       local_x                       => local_x,
       paddr                         => paddr,
